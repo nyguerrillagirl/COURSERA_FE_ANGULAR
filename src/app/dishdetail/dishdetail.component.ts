38a011eca4ba7537b1a341979491c1a2bc99dda6
Lorraine Figueroa <nyguerrillagirl@brainycode.com>
2021-11-18 21:25:03 -0500

    Week 4 - Animations Part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -8,11 +8,25 @@ import { switchMap } from 'rxjs/operators';
 
 import { Comment } from '../shared/comment';
 import { MatSlider } from '@angular/material';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+      state('shown', style({
+        transform: 'scale(1.0)',
+        opacity: 1
+      })),
+      state('hidden', style({
+        transform: 'scale(0.5)',
+        opacity: 0
+      })),
+      transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
 
@@ -25,6 +39,7 @@ export class DishdetailComponent implements OnInit {
   commentForm: FormGroup;
   customerComment: Comment;
   dishCopy: Dish;
+  visibility: string;
 
   @ViewChild('cform') commentFormDirective;
 
@@ -62,8 +77,8 @@ export class DishdetailComponent implements OnInit {
     this.dishService.getDishIds()
       .subscribe((dishIds) => this.dishIds = dishIds);
     this.route.params
-      .pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
-      .subscribe((dish) => {this.dish = dish; this.dishCopy = dish; this.setPrevNext(dish.id);},
+      .pipe(switchMap((params: Params) => { this.visibility = 'hidden'; return this.dishService.getDish(params['id']);}))
+      .subscribe((dish) => {this.dish = dish; this.dishCopy = dish; this.setPrevNext(dish.id); this.visibility = 'shown';},
        errmess => this.errMess = <any>errmess);
   }
 
